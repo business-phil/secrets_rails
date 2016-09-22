4830be092b0302147a65f0df4b35a2afa3dded03
Phil Boothe <pfb3cn@virginia.edu>
2016-09-22 19:15:24 -0400

    added user view and tests

--- spec/views/users/view.html.erb_spec.rb
+++ spec/views/users/view.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "users/view.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'user profile page' do
+    before do
+        @user = create_user
+        log_in @user
+    end
+    it 'displays information about the user' do
+        expect(page).to have_text("#{@user.name}")
+        expect(page).to have_link("Edit Profile")
+    end
 end
